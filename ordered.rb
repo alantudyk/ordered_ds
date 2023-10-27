3cf8db35c657895e53b1b15dc7d4dfd4297b6e15
Reznov <78517110+alantudyk@users.noreply.github.com>
2023-10-27 14:17:30 +0300

    >>

--- ordered.rb
+++ ordered.rb
@@ -1,24 +1,50 @@
-#!/bin/ruby
+class PQ
 
-require 'algorithms'
+    def initialize(array = [], heapify = true, &is_unordered)
+        raise ArgumentError.new 'PQ init' unless array.class == Array &&
+            (heapify == true || heapify == false) && block_given?
+        @a, @z, @u = array, array.size, is_unordered
+        @z.times.reverse_each { sink _1 } if heapify
+    end
 
-class OMap < Containers::CRBTreeMap
-    def initialize(d = nil) = (@d = d)
+    def top = @z > 0 ? @a.first : nil
 
-    def [](k) = has_key?(k) ? get(k) : @d
+    def << x
+        @a[i = @z] = x
+        @z += 1
+        while i > 0
+            p = (i - (i[0] ^ 1)) / 2
+            break unless @u.call @a[p], @a[i]
+            @a[p], @a[i] = @a[i], @a[p]
+            i = p
+        end
+    end
+
+    def pop
+        return nil unless r = top
+        @a[0] = @a[@z -= 1]
+        sink 0
+        r
+    end
+
+    private
+
+    def sink p
+        while (c = p * 2 + 1) < @z
+            c += 1 if c.n < @z && @u.(@a[c], @a[c.n])
+            break unless @u.call @a[p], @a[c]
+            @a[p], @a[c] = @a[c], @a[p]
+            p = c
+        end
+    end
+
 end
 
-class OMSet < Containers::CRBTreeMap
+class MinQ < PQ
+    def initialize(a = [], h = true) = super(a, h) { _1 > _2 }
 end
 
-class OSet < OMSet
+class MaxQ < PQ
+    def initialize(a = [], h = true) = super(a, h) { _1 < _2 }
 end
 
-class MinQ
-end
-
-class MaxQ
-end
-
-m = OMap.new 0
-p [m[1] += 1, m.size]
